1a74174e7c274c22c30092c55dcccf0055001754
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2024-08-26 01:06:55 -0500

    Cracion del footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,32 @@
+<footer class="bg-[#263238]">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-6xl px- sm:px-6 lg:px-8 mx-auto text-center">
+        <div class="grid grid-flow-row md:grid-flow-col justify-items-center items-center min-h-20 py-6">            
+
+            <div class="text-white whitespace-pre-wrap">
+Contáctenos 
+                
+Correo electrónico: examplo@gmail.com 
+
+Celular: 947 299 925
+            </div>
+
+            <div class="text-white whitespace-pre-wrap">
+Términos y condiciones
+
+Política de privacidad
+            </div>           
+           
+        </div>
+
+        <div class="text-white pt-2 pb-6 ">
+            © 2024 h. Todos los derechos reservados.
+
+        </div>
+
+
+    </div>
+
+   
+    
+</footer>
