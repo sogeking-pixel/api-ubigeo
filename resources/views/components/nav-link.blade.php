bf8b05b87eb1816de8df6358319c70722e476947
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2024-08-26 20:19:55 -0500

    Actualizo el diseno del los link nav

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-base font-semibold leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-sky-600 text-base font-semibold leading-5 text-gray-900 focus:outline-none border-sky-600 transition duration-150 ease-in-out'
             : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-base font-medium hover:font-semibold leading-5 text-gray-600 hover:text-gray-800 hover:border-gray-400 focus:outline-none focus:text-gray-700 focus:border-gray-400 transition duration-150 ease-in-out';
 @endphp
 
